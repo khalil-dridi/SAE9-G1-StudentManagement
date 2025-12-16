7e792d8f3b646b46eb212e992a6022e14e082158
khalil-dridi <khalil.dridi@esprit.tn>
2025-12-16 05:28:23 +0100

    testing for webhooks

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentManagementApplication {
+    //test
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
